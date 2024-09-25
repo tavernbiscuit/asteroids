dce4461b7f843547a483668f66b1c4172e6a108a
tavernbiscuit <eclange12@gmail.com>
2024-09-25 14:01:33 -0500

    Added CirlceShape parent class and Player child class. Player class includes triangle and draw methods. The latter overrides parent method. Added Player Radius to constants. Adjusted main.py to include the creation of new Player class object.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    running = True
-    while running:
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+
+      
 
 if __name__ == "__main__":
     main()
