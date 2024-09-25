bbe6a54f9448c69f3176809854e9be1800212e8a
tavernbiscuit <eclange12@gmail.com>
2024-09-25 14:47:32 -0500

    created new groups to hold all objects of Player class created.

--- main.py
+++ main.py
@@ -8,20 +8,28 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+
+    Player.containers = (updatable_group, drawable_group)
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0,0,0))
-        player.draw(screen)
+        screen.fill("black")
+
+        for player in drawable_group:
+            player.draw(screen)
+            
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
-        player.update(dt)
+        for player in updatable_group:
+            player.update(dt)
 
       
 
--- player.py
+++ player.py
@@ -10,7 +10,10 @@ class Player(CircleShape):
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        right = (
+            pygame.Vector2(0, 1).rotate(self.rotation + 90) 
+            * self.radius / 1.5
+        )
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
